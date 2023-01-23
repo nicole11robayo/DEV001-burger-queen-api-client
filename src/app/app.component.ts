45058de8fa75811c76c26442bb9db6e7926f18f0
camila leal <khammylv@gmail.com>
2023-01-23 09:56:41 -0500

    instalacion angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dev001-burger-queen-api-client';
+}
