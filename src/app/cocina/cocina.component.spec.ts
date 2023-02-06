ba8854d67d12b689a7d622b942f849e142137144
camila leal <khammylv@gmail.com>
2023-02-06 12:41:58 -0500

    test y form

--- src/app/cocina/cocina.component.spec.ts
+++ src/app/cocina/cocina.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CocinaComponent } from './cocina.component';
@@ -8,7 +9,9 @@ describe('CocinaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CocinaComponent ]
+      imports: [HttpClientModule],
+      providers: [CocinaComponent ]
+    
     })
     .compileComponents();
 
